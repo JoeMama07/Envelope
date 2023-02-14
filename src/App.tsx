1ff47dfacd6709fa1a2b53882069a3c85795af08
Josh <josh.k@tad.sr>
2023-02-14 12:58:03 -0300

    feat: add framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { motion } from "framer-motion";
 import "./App.css";
 import Envelope from "./components/Envelope";
 
 function App() {
   return (
-    <div className="flex justify-center mt-20">
+    <motion.div
+      initial={{
+        y: 100,
+        opacity: 0,
+      }}
+      whileInView={{
+        y: 0,
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      className="flex justify-center mt-20"
+    >
       <Envelope />
-    </div>
+    </motion.div>
   );
 }
 
